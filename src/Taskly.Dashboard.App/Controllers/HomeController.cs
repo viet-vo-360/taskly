bac2775aaa585cc6ccebea36c66ea21e40cab739
Huy Vu <vohuyvu360@gmail.com>
2025-03-25 08:45:20 +0700

    03: Update HomeController.cs file.

--- src/Taskly.Dashboard.App/Controllers/HomeController.cs
+++ src/Taskly.Dashboard.App/Controllers/HomeController.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BCrypt.Net;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
 using Taskly.Dashboard.App.Data;
 using Taskly.Users.Models;
-using System.Security.Cryptography;
-using BCrypt.Net;
 
 namespace Home.Controllers
 {
