b3e8b4e6e22137fbdc0e9c7f6272c9e4ff330949
Huy Vu <vohuyvu360@gmail.com>
2025-03-25 09:15:08 +0700

    03: Update AppDbContext.cs file

--- src/Taskly.Dashboard.App/Data/AppDbContext.cs
+++ src/Taskly.Dashboard.App/Data/AppDbContext.cs
@@ -18,9 +18,6 @@ public partial class AppDbContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Name=DefaultConnection");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>(entity =>
